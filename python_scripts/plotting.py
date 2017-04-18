Fixed nan issue in tau_c mean calculation

python_scripts/plotting.py
@@ -8,16 +8,16 @@ final analysis and plot the results.
 """
 
 # Import modules
-from helpers import read_netcdf_dataset, get_config, get_pp_fn, save_fig_and_log
+from helpers import read_netcdf_dataset, get_config, save_fig_and_log
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 # Define functions
-def plot_domain_mean_weather_ts(inargs):
+def plot_domain_mean_precipitation_ts(inargs):
     """
-    Function to plot time series of domain mean weather for each day
+    Function to plot time series of domain mean precipitation for each day
     
     Parameters
     ----------
@@ -83,12 +83,12 @@ def plot_domain_mean_weather_ts(inargs):
     plt.tight_layout()
 
     # Save figure
-    save_fig_and_log(fig, rootgroup, inargs, 'weather_ts_individual')
+    save_fig_and_log(fig, rootgroup, inargs, 'precipitation_ts_individual')
 
 
-def plot_domain_mean_weather_ts_composite(inargs):
+def plot_domain_mean_precipitation_ts_composite(inargs):
     """
-    Function to plot time series of domain mean weather as a composite over 
+    Function to plot time series of domain mean precipitation as a composite over 
     all days
 
     Parameters
@@ -130,7 +130,84 @@ def plot_domain_mean_weather_ts_composite(inargs):
     plt.tight_layout()
 
     # Save figure and log
-    save_fig_and_log(fig, rootgroup, inargs, 'weather_ts_composite')
+    save_fig_and_log(fig, rootgroup, inargs, 'precipitation_ts_composite')
+
+
+def plot_domain_mean_cape_tauc_ts(inargs):
+    """
+    Function to plot time series of domain mean CAPE abd tau_c for each day
+
+    Parameters
+    ----------
+    inargs : argparse object
+      Argparse object with all input arguments
+
+    Returns
+    -------
+
+    """
+
+    # Read pre-processed data
+    rootgroup = read_netcdf_dataset(inargs)
+    n_days = rootgroup.dimensions['date'].size
+    x = rootgroup.variables['time'][:]
+
+    # Set up figure
+    n_cols = 4
+    n_rows = int(np.ceil(float(n_days) / n_cols))
+
+    fig, axmat = plt.subplots(n_rows, n_cols, sharex=True, sharey=True,
+                              figsize=(10,3*n_rows))   # TODO: figsize
+    axflat = np.ravel(axmat)
+
+    # Loop over axes / days
+    for iday in range(n_days):
+        ax1 = axflat[iday]
+        ax2 = ax1.twinx()
+
+        dateobj = (timedelta(seconds=int(rootgroup.variables['date'][iday])) +
+                   datetime(1,1,1))
+        datestr = dateobj.strftime(get_config(inargs, 'colors', 'date_fmt'))
+        ax1.set_title(datestr)
+        if iday % 4 == 0:   # Only left column
+            ax1.set_ylabel('CAPE [J/kg]')
+        if iday % 4 == 3:
+            ax2.set_ylabel('tau_c [h]')
+        if iday >= ((n_cols * n_rows) - n_cols):   # Only bottom row
+            ax1.set_xlabel('Time [UTC]')
+
+        for group in ['det', 'ens']:
+            for var, ax in zip(['CAPE_ML', 'TAU_C'], [ax1, ax2]):
+                # Get data do be plotted
+                # prec: det, obs or ens mean
+                # prec_lower/upper: ensemble minimum, maximum
+                if group == 'ens':
+                    array = rootgroup.groups[group].variables[var]\
+                        [iday, :, :]
+                    mean = np.mean(array, axis=1)
+                    lower = np.amin(array, axis=1)
+                    upper = np.amax(array, axis=1)
+                else:
+                    mean = rootgroup.groups[group].variables[var]\
+                        [iday, :, 0]
+
+                # Plot data
+                ax.plot(x, mean, label=group,
+                                  c=get_config(inargs, 'colors', group))
+                if group == 'ens':
+                    ax.fill_between(x, lower, upper,
+                                              where=upper >= lower,
+                                              facecolor=get_config(inargs,
+                                                                   'colors',
+                                                                   'ens_range'))
+
+    # Finish figure
+    axflat[0].legend(loc=0)
+
+    plt.tight_layout()
+
+    # Save figure
+    save_fig_and_log(fig, rootgroup, inargs, 'cape_tauc_ts_individual')
 
 
 def plotting(inargs):
@@ -148,5 +225,7 @@ def plotting(inargs):
     """
 
     # Call appropriate plotting function
-    plot_domain_mean_weather_ts(inargs)
-    plot_domain_mean_weather_ts_composite(inargs)
+    #plot_domain_mean_precipitation_ts(inargs)
+    #plot_domain_mean_precipitation_ts_composite(inargs)
+
+    plot_domain_mean_cape_tauc_ts(inargs)