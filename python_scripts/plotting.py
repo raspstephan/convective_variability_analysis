fc6608b8f946d807e2c5b36e99ff963764796d5c
raspstephan <raspstephan@gmail.com>
2017-04-18 10:15:15 +0200

    Added cape_tauc individual plots

--- python_scripts/plotting.py
+++ python_scripts/plotting.py
@@ -15,67 +15,47 @@ import numpy as np
 
 
 # Define functions
-def plot_domain_mean_precipitation_ts(inargs):
+def plot_domain_mean_timeseries_individual(inargs, type):
     """
     Function to plot time series of domain mean precipitation for each day
-    
+
     Parameters
     ----------
     inargs : argparse object
       Argparse object with all input arguments
-
-    Returns
-    -------
+    type : str
+      Type of plot. Must be 'precipitation' or 'cape_tauc'
 
     """
+
+    assert type in ['precipitation', 'cape_tauc'], \
+        'Type must be precipitation or cape_tauc'
 
     # Read pre-processed data
     rootgroup = read_netcdf_dataset(inargs)
     n_days = rootgroup.dimensions['date'].size
-    x = rootgroup.variables['time'][:]
 
     # Set up figure
     n_cols = 4
     n_rows = int(np.ceil(float(n_days) / n_cols))
 
     fig, axmat = plt.subplots(n_rows, n_cols, sharex=True, sharey=True,
-                              figsize=(10,3*n_rows))   # TODO: figsize
+                              figsize=(10, 3 * n_rows))
     axflat = np.ravel(axmat)
 
     # Loop over axes / days
     for iday in range(n_days):
         dateobj = (timedelta(seconds=int(rootgroup.variables['date'][iday])) +
-                   datetime(1,1,1))
-        datestr = dateobj.strftime(get_config(inargs, 'colors', 'date_fmt'))
+                   datetime(1, 1, 1))
+        datestr = dateobj.strftime(get_config(inargs, 'plotting', 'date_fmt'))
         axflat[iday].set_title(datestr)
-        if iday % 4 == 0:   # Only left column
-            axflat[iday].set_ylabel('Accumulation [mm/h]')
-        if iday >= ((n_cols * n_rows) - n_cols):   # Only bottom row
+        if iday >= ((n_cols * n_rows) - n_cols):  # Only bottom row
             axflat[iday].set_xlabel('Time [UTC]')
 
-        for group in rootgroup.groups:
-            # Get data do be plotted
-            # prec: det, obs or ens mean
-            # prec_lower/upper: ensemble minimum, maximum
-            if group == 'ens':
-                prec_array = rootgroup.groups[group].variables['PREC_ACCUM']\
-                    [iday, :, :]
-                prec = np.mean(prec_array, axis=1)
-                prec_lower = np.amin(prec_array, axis=1)
-                prec_upper = np.amax(prec_array, axis=1)
-            else:
-                prec = rootgroup.groups[group].variables['PREC_ACCUM']\
-                    [iday, :, 0]
-
-            # Plot data
-            axflat[iday].plot(x, prec, label=group,
-                              c=get_config(inargs, 'colors', group))
-            if group == 'ens':
-                axflat[iday].fill_between(x, prec_lower, prec_upper,
-                                          where=prec_upper >= prec_lower,
-                                          facecolor=get_config(inargs,
-                                                               'colors',
-                                                               'ens_range'))
+        if type == 'precipitaiton':
+            plot_precipitation_panel(inargs, axflat, iday, rootgroup)
+        if type == 'cape_tauc':
+            plot_cape_tauc_panel(inargs, axflat, iday, rootgroup)
 
     # Finish figure
     axflat[0].legend(loc=0)
@@ -83,7 +63,113 @@ def plot_domain_mean_precipitation_ts(inargs):
     plt.tight_layout()
 
     # Save figure
-    save_fig_and_log(fig, rootgroup, inargs, 'precipitation_ts_individual')
+    save_fig_and_log(fig, rootgroup, inargs, type + '_ts_individual')
+
+
+def plot_precipitation_panel(inargs, axflat, iday, rootgroup):
+    """
+    Plots precipitation timeseries in each panel.
+    
+    Parameters
+    ----------
+    inargs : argparse object
+      Argparse object with all input arguments
+    axflat : list
+      List of axis objects
+    iday : int
+      Index for list object
+    rootgroup : netCDF object
+      NetCDF object with data to plot
+
+    """
+
+    dateobj = (timedelta(seconds=int(rootgroup.variables['date'][iday])) +
+               datetime(1, 1, 1))
+    datestr = dateobj.strftime(get_config(inargs, 'colors', 'date_fmt'))
+    axflat[iday].set_title(datestr)
+
+    for group in rootgroup.groups:
+        # Get data do be plotted
+        # prec: det, obs or ens mean
+        # prec_lower/upper: ensemble minimum, maximum
+        if group == 'ens':
+            prec_array = rootgroup.groups[group].variables['PREC_ACCUM'] \
+                [iday, :, :]
+            prec = np.mean(prec_array, axis=1)
+            prec_lower = np.amin(prec_array, axis=1)
+            prec_upper = np.amax(prec_array, axis=1)
+        else:
+            prec = rootgroup.groups[group].variables['PREC_ACCUM'] \
+                [iday, :, 0]
+
+        # Plot data
+        axflat[iday].plot(rootgroup.variables['time'][:], prec, label=group,
+                          c=get_config(inargs, 'colors', group))
+        if group == 'ens':
+            axflat[iday].fill_between(rootgroup.variables['time'][:],
+                                      prec_lower, prec_upper,
+                                      where=prec_upper >= prec_lower,
+                                      facecolor=get_config(inargs,
+                                                           'colors',
+                                                           'ens_range'))
+
+
+def plot_cape_tauc_panel(inargs, axflat, iday, rootgroup):
+    """
+    Plots cape and tau_c timeseries in each panel.
+
+    Parameters
+    ----------
+    inargs : argparse object
+      Argparse object with all input arguments
+    axflat : list
+      List of axis objects
+    iday : int
+      Index for list object
+    rootgroup : netCDF object
+      NetCDF object with data to plot
+
+    """
+
+    ax1 = axflat[iday]
+    ax2 = ax1.twinx()
+    ax2.set_ylim(0,20)
+
+    dateobj = (timedelta(seconds=int(rootgroup.variables['date'][iday])) +
+               datetime(1, 1, 1))
+    datestr = dateobj.strftime(get_config(inargs, 'plotting', 'date_fmt'))
+    ax1.set_title(datestr)
+    if iday % 4 == 0:  # Only left column
+        ax1.set_ylabel('CAPE [J/kg]')
+    if iday % 4 == 3:
+        ax2.set_ylabel('tau_c [h]')
+    else:
+        ax2.get_yaxis().set_ticks([])
+
+    for group in ['det', 'ens']:
+        for var, ax, ls in zip(['CAPE_ML', 'TAU_C'],
+                               [ax1, ax2],
+                               ['-', '--']):
+            # Get data do be plotted
+            # prec: det, obs or ens mean
+            # prec_lower/upper: ensemble minimum, maximum
+            if group == 'ens':
+                array = rootgroup.groups[group].variables[var][iday, :, :]
+                mean = np.mean(array, axis=1)
+                lower = np.amin(array, axis=1)
+                upper = np.amax(array, axis=1)
+            else:
+                mean = rootgroup.groups[group].variables[var][iday, :, 0]
+
+            # Plot data
+            ax.plot(rootgroup.variables['time'][:], mean, label=group,
+                    c=get_config(inargs, 'colors', group), ls=ls)
+            if group == 'ens':
+                ax.fill_between(rootgroup.variables['time'][:],
+                                lower, upper,
+                                where=upper >= lower,
+                                facecolor=get_config(inargs, 'colors',
+                                                     'ens_range'))
 
 
 def plot_domain_mean_precipitation_ts_composite(inargs):
@@ -133,81 +219,7 @@ def plot_domain_mean_precipitation_ts_composite(inargs):
     save_fig_and_log(fig, rootgroup, inargs, 'precipitation_ts_composite')
 
 
-def plot_domain_mean_cape_tauc_ts(inargs):
-    """
-    Function to plot time series of domain mean CAPE abd tau_c for each day
 
-    Parameters
-    ----------
-    inargs : argparse object
-      Argparse object with all input arguments
-
-    Returns
-    -------
-
-    """
-
-    # Read pre-processed data
-    rootgroup = read_netcdf_dataset(inargs)
-    n_days = rootgroup.dimensions['date'].size
-    x = rootgroup.variables['time'][:]
-
-    # Set up figure
-    n_cols = 4
-    n_rows = int(np.ceil(float(n_days) / n_cols))
-
-    fig, axmat = plt.subplots(n_rows, n_cols, sharex=True, sharey=True,
-                              figsize=(10,3*n_rows))   # TODO: figsize
-    axflat = np.ravel(axmat)
-
-    # Loop over axes / days
-    for iday in range(n_days):
-        ax1 = axflat[iday]
-        ax2 = ax1.twinx()
-
-        dateobj = (timedelta(seconds=int(rootgroup.variables['date'][iday])) +
-                   datetime(1,1,1))
-        datestr = dateobj.strftime(get_config(inargs, 'colors', 'date_fmt'))
-        ax1.set_title(datestr)
-        if iday % 4 == 0:   # Only left column
-            ax1.set_ylabel('CAPE [J/kg]')
-        if iday % 4 == 3:
-            ax2.set_ylabel('tau_c [h]')
-        if iday >= ((n_cols * n_rows) - n_cols):   # Only bottom row
-            ax1.set_xlabel('Time [UTC]')
-
-        for group in ['det', 'ens']:
-            for var, ax in zip(['CAPE_ML', 'TAU_C'], [ax1, ax2]):
-                # Get data do be plotted
-                # prec: det, obs or ens mean
-                # prec_lower/upper: ensemble minimum, maximum
-                if group == 'ens':
-                    array = rootgroup.groups[group].variables[var]\
-                        [iday, :, :]
-                    mean = np.mean(array, axis=1)
-                    lower = np.amin(array, axis=1)
-                    upper = np.amax(array, axis=1)
-                else:
-                    mean = rootgroup.groups[group].variables[var]\
-                        [iday, :, 0]
-
-                # Plot data
-                ax.plot(x, mean, label=group,
-                                  c=get_config(inargs, 'colors', group))
-                if group == 'ens':
-                    ax.fill_between(x, lower, upper,
-                                              where=upper >= lower,
-                                              facecolor=get_config(inargs,
-                                                                   'colors',
-                                                                   'ens_range'))
-
-    # Finish figure
-    axflat[0].legend(loc=0)
-
-    plt.tight_layout()
-
-    # Save figure
-    save_fig_and_log(fig, rootgroup, inargs, 'cape_tauc_ts_individual')
 
 
 def plotting(inargs):
@@ -228,4 +240,4 @@ def plotting(inargs):
     #plot_domain_mean_precipitation_ts(inargs)
     #plot_domain_mean_precipitation_ts_composite(inargs)
 
-    plot_domain_mean_cape_tauc_ts(inargs)
+    plot_domain_mean_timeseries_individual(inargs, type='cape_tauc')
